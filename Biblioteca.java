346156fe994f0d00c86c7a61366ad95241428790
Pedro Almeida <pedro15almeida12@gmail.com>
2025-08-07 13:55:26 -0300

    Criando a classe livro e seus métodos

---
+++ Biblioteca.java
@@ -0,0 +1,12 @@
+public class Biblioteca {
+    public static void main(String[] args) {
+        Livro livro1 = new Livro();
+        livro1.setTitulo("Diario de um Banana");
+        livro1.setAnoDePublicacao(2006);
+        livro1.setAutor("Pedrinho");
+
+        livro1.exibirDetalhes();
+    }
+}
+
+
